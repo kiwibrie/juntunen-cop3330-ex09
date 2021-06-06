ffcfd3acbe4c23be5cd4e39a09faa901eb8c0d2e
kiwibrie <brianne0128@gmail.com>
2021-06-05 20:37:11 -0500

    base case working

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -13,6 +13,30 @@ public class App {
         App myApp = new App();
         //( l * w )/ 350
 
-        
+        int length = myApp.prompt("length");
+        int width = myApp.prompt("width");
+        int area = length * width;
+        final int galCov = 350;
+
+        int gallons = myApp.calcCoverage(area, galCov);
+
+        myApp.printOutput(area, gallons);
+    }
+
+    public void printOutput(int area, int gallons){
+        System.out.printf("You will need to purchase %d gallons " +
+                "of paint to cover %d square feet.", gallons, area);
+    }
+
+    public int prompt(String prompt){
+        System.out.printf("What is the %s of the room? ", prompt);
+        return in.nextInt();
+    }
+
+    public int calcCoverage(int area, int galCov){
+        if (area % galCov > 0){
+            return area/galCov +1;
+        }
+        return area/galCov;
     }
 }
